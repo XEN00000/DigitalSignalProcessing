2de7d4423d1b678d671519f2f5329164fea8a0b0
Aleksander <251544@edu.p.lodz.pl>
2026-03-05 19:19:11 +0100

    Refactor noise generator classes for improved clarity and structure

--- generators/NoiseGenerators.py
+++ generators/NoiseGenerators.py
@@ -1,7 +1,23 @@
 import numpy as np
 from generators.BaseGenerator import BaseGenerator
 
+
 class UniformNoiseGenerator(BaseGenerator):
     def generate(self):
         # 1 / (high - low)
         return np.random.uniform(-self.A, self.A, size=len(self.time_axis))
+
+
+class GaussianNoiseGenerator(BaseGenerator):
+    def generate(self):
+        mu = 0.0
+        sigma = 1.0
+
+        num_samples = len(self.time_axis)
+
+        noise = np.random.normal(loc=mu, scale=sigma, size=num_samples)
+
+        # scaling by amplitude
+        signal = self.A * noise
+
+        return signal
